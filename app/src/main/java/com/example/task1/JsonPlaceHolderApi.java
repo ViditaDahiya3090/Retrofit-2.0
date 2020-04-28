26be8fbfb4fc029894aa3cf20b8ff693c3066dfa
ViditaDahiya3090 <viditadahiya3090@gmail.com>
2020-04-28 18:49:00 +0530

    Initial commit

---
+++ app/src/main/java/com/example/task1/JsonPlaceHolderApi.java
@@ -0,0 +1,16 @@
+package com.example.task1;
+
+import java.util.List;
+
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+
+public interface JsonPlaceHolderApi {
+
+    @GET("posts")
+    Call<List<Post>> getPosts();
+
+   }
+
